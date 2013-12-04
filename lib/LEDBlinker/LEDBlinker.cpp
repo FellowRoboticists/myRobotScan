02374a47b0d409c899360ab3d23a0aaf59bbc6eb
Dave Sieh <dj0hnve@gmail.com>
2013-12-03 17:21:29 -0700

    Added additional blinker methods.

--- lib/LEDBlinker/LEDBlinker.cpp
+++ lib/LEDBlinker/LEDBlinker.cpp
@@ -9,13 +9,39 @@ LEDBlinker::LEDBlinker(int ledPin) {
   pin = ledPin;
 }
 
-void LEDBlinker::blinkNumber(int number) {
-  pinMode(pin, OUTPUT); // enable the pin for output
+void LEDBlinker::begin() {
+  pinMode(pin, OUTPUT);
+}
 
+void LEDBlinker::blinkNumber(int number) {
   while (number--) {
     digitalWrite(pin, HIGH); 
     delay(100);
     digitalWrite(pin, LOW); 
     delay(400);
+  }
+}
+
+void LEDBlinker::flash(int percent) {
+  Serial.print(", flash percent = ");
+  Serial.println(percent);
+  const int duration = 1000;
+  // Blink the LED
+  digitalWrite(pin, HIGH);
+  int onTime = map(percent, 0, 100, 0, duration);
+  delay(onTime);
+  digitalWrite(pin, LOW);
+  delay(duration - onTime);
+}
+
+void LEDBlinker::flashCritical() {
+  for (int i = 0; i < 3; i++) {
+    flash(20);
+  }
+  for (int i = 0; i < 3; i++) {
+    flash(60);
+  }
+  for (int i = 0; i < 3; i++) {
+    flash(20);
   }
 }
--- lib/LEDBlinker/LEDBlinker.h
+++ lib/LEDBlinker/LEDBlinker.h
@@ -17,11 +17,22 @@ class LEDBlinker {
    */
   LEDBlinker(int ledPin);
 
+  void begin();
+
   /**
      This will cause the LED to blink the number of times specified.
    */
   void blinkNumber(int number);
 
+  /**
+     Percent is the percent of on time time (duty cycle)
+  */
+  void flash(int percent);
+
+  /**
+     Flashes SOS in morse code.
+  */
+  void flashCritical();
  private:
   int pin;
 };
